e548f22cd86df7009eea7852bf4c54a012a4b275
saikumarkindigerDev <saikumarkindigeri171@gmail.com>
2024-02-11 10:22:13 +0530

    shiva

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ console.log({token})
 
 })
 
-const PORT = process.env.PORT ||  3000
+const PORT = process.env.PORT ||4000
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
