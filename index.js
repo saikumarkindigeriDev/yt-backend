4a0d4c44004de816fcc6b75464d03a51af7c38aa
saikumarkindigerDev <saikumarkindigeri171@gmail.com>
2024-02-11 10:13:48 +0530

    reupdate

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ console.log({token})
 
 })
 
-const PORT = process.env.PORT ||  9000
+const PORT = process.env.PORT ||  3000
 
 app.listen(PORT, () => {
-    console.log("Server is running....")
+    console.log(`Server is running on ${PORT}`)
 })
